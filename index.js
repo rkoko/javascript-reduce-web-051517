dc3a526aa7126bcd2248350ac5d18eb931190a26
rkoko <rinda.ko@gmail.com>
2017-06-27 23:19:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const app = "I don't do much."
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+
+  return result;
+}
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+
+console.log(reduce(couponLocations, couponCounter, 0));
